0991db00294cc62cb34f758f78f6e183918556f4
thutyecology <43251726+thutyecology@users.noreply.github.com>
2023-07-03 10:48:56 +0800

    Add files via upload

---
+++ code/deep_learning/alexnet/generate_sample.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 17:42:12 2023
+
+@author: Ying Tu
+"""
+
+import os
+import cv2
+from dbfread import DBF
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
+from shutil import copy, rmtree
+
+def mk_file(file_path: str):
+    if os.path.exists(file_path):
+        rmtree(file_path)
+    os.makedirs(file_path)
+
+def clear_dir(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            
+data_root = os.path.abspath(os.path.join(os.getcwd(), "../../.."))
+
+img1 = cv2.imread(data_root+'/data/Fig1_1939.jpeg')
+img2 = cv2.imread(data_root+'/data/Fig2_2019.jpeg')
+img1gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+img2rgb = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+
+img_size = 32
+
+m,n,d = img2.shape
+newrow = m+img_size
+newcol = n+img_size
+newimg  = np.zeros([newrow,newcol,d], dtype=np.uint8)
+for b in range(0,d):
+    start = int(img_size/2)
+    newimg[start:start+m,start:start+n,b] = img2[:,:,b]
+
+fig,ax = plt.subplots(1,1,dpi=300)
+plt.imshow(cv2.cvtColor(newimg, cv2.COLOR_BGR2RGB))
+
+sample_file = os.path.join(data_root,'data','sample','sample_2019_location.dbf')
+dfsample = pd.DataFrame(DBF(sample_file))
+class_dict = {
+    1: "Cropland",
+    2: "Built-up (Low)",
+    3: "Built-up (High)",
+    4: "Vegetation",
+    5: "Bareland"
+}    
+
+sampledir = os.path.join(data_root,'data','img2_rgb_size'+str(img_size),'sample')
+mk_file(sampledir)
+clear_dir(sampledir)
+
+for label in range(1,len(class_dict)+1):
+    classname = class_dict[label]
+
+    classdir = os.path.join(data_root,'data','img2_rgb_size'+str(img_size),'sample','class'+str(label))
+    mk_file(classdir)
+    
+    dfclass = dfsample.loc[dfsample['Label']==label]
+    values = dfclass[['Row','Column']].values
+    m,n = values.shape
+    for i in range(0,m):
+        row = values[i][0]
+        col = values[i][1]
+        subset = newimg[row:row+img_size,col:col+img_size,:]
+        outname = 'row'+str(row)+'_col'+str(col)+'.jpg'
+        cv2.imwrite(os.path.join(classdir,outname), subset)
+print ('Sample data created!')      
---
+++ code/deep_learning/alexnet/predict.py
@@ -0,0 +1,111 @@
+import os
+import sys
+import torch
+import json
+from torchvision import transforms, datasets
+from tqdm import tqdm
+import cv2
+import numpy as np
+
+from model import AlexNet
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    input_size = 32
+
+    data_transform = transforms.Compose(
+        [
+         transforms.Resize((input_size, input_size)),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # Load model
+    model = AlexNet(num_classes=5).to(device)
+    weights_path = './AlexNet'+str(input_size)+'.pth'
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path))
+    model.eval()
+
+    # Load data path
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../../.."))
+    image_path = os.path.join(data_root, "data", "img2_rgb_size" + str(input_size))
+    all_path = os.path.join(image_path, "all")
+
+    # Load validation data
+    validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                            transform=data_transform)
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=4, shuffle=False,
+                                                  num_workers=2)
+
+    # Compute validation accuracy
+    acc = 0.0  # accumulate accurate number / epoch
+    with torch.no_grad():
+        val_bar = tqdm(validate_loader, file=sys.stdout)
+        for val_data in val_bar:
+            val_images, val_labels = val_data
+            outputs = model(val_images.to(device))
+            predict_y = torch.max(outputs, dim=1)[1]
+            acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+    val_accurate = acc / val_num
+
+    acc_dict = {'accuracy': val_accurate}
+    json_str = json.dumps(acc_dict)
+    with open('accuracy.json', 'w') as json_file:
+        json_file.write(json_str)
+
+    print()
+    print("Model: AlexNet")
+    print("Validation accuracy: %.2f" % (val_accurate))
+
+    # Load image
+    org_path = os.path.join(data_root, "data", "Fig2_2019.jpeg")
+    img2 = cv2.imread(org_path)
+    img2rgb = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+    m, n, d = img2.shape
+
+    # Expand image
+    img2expand = np.zeros([m + input_size, n + input_size, d], dtype=np.uint8)
+    for b in range(0, d):
+        start = int(input_size / 2)
+        img2expand[start:start + m, start:start + n, b] = img2rgb[:, :, b]
+    
+    # Predict land cover types
+    print()
+    print("#############Execute prediction#############")
+    outimg = np.zeros([m, n], dtype=np.int8)
+    for i in range(0, m, 1):
+        for j in range(0, n, 1):
+
+            img = img2expand[i:i+input_size, j:j+input_size,:]
+
+            # [N, C, H, W]
+            img = transforms.ToPILImage()(img)
+            img = data_transform(img)
+
+            # Expand batch dimension
+            img = torch.unsqueeze(img, dim=0)
+
+            with torch.no_grad():
+                # Predict class
+                output = torch.squeeze(model(img.to(device))).cpu()
+                predict = torch.softmax(output, dim=0)
+                predict_cla = torch.argmax(predict).numpy()
+
+            outimg[i][j] = predict_cla
+
+        print('Row ' + str(i) + ' finished!')
+
+    # Save predictions
+    outname = "predict_5class.tif"
+    outfile = os.path.join(image_path, outname)
+    cv2.imwrite(outfile, outimg)
+    print()
+    print(outname + ' saved!')
+    print("#############End of prediction#############")
+
+if __name__ == '__main__':
+    main()
